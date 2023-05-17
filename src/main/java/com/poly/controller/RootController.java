9992582cac32687d7fc774d96e211858756b78ab
AlexNguyen <nguyenhoainam121n@gmail.com>
2023-05-17 14:58:48 +0700

    first commit

---
+++ src/main/java/com/poly/controller/RootController.java
@@ -0,0 +1,12 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class RootController {
+	@RequestMapping("/")
+	public String index() {
+		return "index.html";
+	}
+}
