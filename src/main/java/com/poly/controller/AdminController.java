ad591f3f7eb5659e475494fd3925f5bf86f9c444
AlexNguyen <nguyenhoainam121n@gmail.com>
2023-05-26 08:46:30 +0700

    Update template

---
+++ src/main/java/com/poly/controller/AdminController.java
@@ -0,0 +1,16 @@
+package com.poly.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import ch.qos.logback.core.model.Model;
+
+@Controller
+@RequestMapping("/admin")
+public class AdminController {
+    @GetMapping("")
+    public String dashboard(Model m) {
+        return "/admin/index";
+    }
+}
--- src/main/java/com/poly/controller/ShopController.java
+++ src/main/java/com/poly/controller/ShopController.java
@@ -1,18 +1,14 @@
 package com.poly.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
-@RequestMapping("/")
-public class RootController {
-
-	@GetMapping("")
-	public String index(Model m) {
-		// m.addAttribute("msg", "Hoài Nam");
-		return "/client/index";
-	}
-
+@RequestMapping("shop")
+public class ShopController {
+    @GetMapping("")
+    public String index() {
+        return "/client/shop";
+    }
 }
